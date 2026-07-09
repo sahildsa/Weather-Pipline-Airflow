6fa728f162626eae5213708c93e2e01f64e9138d
Sahil Saraswat <sahil.ddesmond@gmail.com>
2026-07-09 17:58:32 +0530

    Feat: Created the python file pipeline.py which downloads the data and store it in s3bucket(simulated using folder

---
+++ dags/pipeline.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import datetime as dt
+from docker.types import Mount
+
+from airflow.sdk import dag
+from airflow.providers.docker.operators.docker import DockerOperator
+
+
+@dag(start_date=dt.datetime(2026, 1, 1), schedule="@daily", catchup=False)
+def weather_pipeline():
+    """A simple weather pipeline that runs a Docker container."""
+    create_data=DockerOperator(
+        task_id="api-worker",
+        image="api-worker:latest",
+        auto_remove='force',
+        mounts=[Mount(source="D:/Airflow_Udemy/result", target="/app/result", type="bind")],
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+    )
+
+    create_data
+
+
+weather_pipeline()
